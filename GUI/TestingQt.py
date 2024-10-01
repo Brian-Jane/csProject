5cde77b66541569dddb95d2f2f798d8616128ae9
yashm006 <yash.2006.work@gmail.com>
2024-10-01 20:08:59 +0530

    Miscellaneous

--- GUI/TestingQt.py
+++ GUI/TestingQt.py
@@ -18,15 +18,15 @@ class MyWindow(QtWidgets.QWidget):
         self.setLayout(v_layout)
         for key, checkbox in D.items():
             print(f"Checkbox name: {key} - Text: {checkbox.text()}")"""
-        Layout=QtWidgets.QGridLayout()
-        self.setLayout(Layout)
-        G.enterRow(Layout,'Task-1',6,folder="Folder-1")
-        G.enterRow(Layout,'Task-2',9,folder="Folder-2")
+        
                 
 app = QtWidgets.QApplication(sys.argv)
 
 window = MyWindow()
 
 window.show()
-
+Layout=QtWidgets.QGridLayout()
+window.setLayout(Layout)
+G.enterRow(Layout,'Task-1',6,folder="Folder-1")
+G.enterRow(Layout,'Task-2',9,folder="Folder-2")
 sys.exit(app.exec_())
