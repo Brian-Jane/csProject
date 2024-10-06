3a8f21e931dbe6fcfe30074d0344538a3fe7a0c0
Ani <brian.3553a7@gmail.com>
2024-10-06 15:17:02 +0530

    changes to guifunc to include  taskobjects

--- Libraries/GUIFunc.py
+++ Libraries/GUIFunc.py
@@ -1,4 +1,3 @@
-from PyQt5.QtCore import Qt
 import sys
 import os
 from PyQt5 import QtWidgets,QtGui
@@ -12,141 +11,39 @@ FONT.setPointSize(10)
 conn=m.connect(user='root',host='LocalHost',database='csp', password='0000')
 
 t=Tasks(conn)
-
-def genCheckbox(data:str,layout:QtWidgets.QBoxLayout, row:int,column:int):
-    checkbox=QtWidgets.QCheckBox(data)
-    checkbox.clicked.connect(lambda:checkbox_clicked(checkbox))
+def genCheckbox(task:taskobject,layout:QtWidgets.QBoxLayout, row:int,column:int):
+    checkbox=QtWidgets.QCheckBox(task.msg)
+    checkbox.clicked.connect(lambda:checkbox_clicked(checkbox,task))
     checkbox.setFont(FONT)
 
     layout.addWidget(checkbox,row,column)
 
-def checkbox_clicked(checkbox:QtWidgets.QCheckBox):
-    L=t.fetchall()
+def checkbox_clicked(checkbox:QtWidgets.QCheckBox,task:taskobject):
     print(checkbox.text(),"is clicked!")
-    t.completeTask(L.ID(checkbox.text()))
+    t.completeTask(task.ID)
 
 def genLabel(data,layout:QtWidgets.QBoxLayout, row:int=None, column:int=None):
     Label=QtWidgets.QLabel(data)
-    if row is None: layout.addWidget(Label)
+    if type(row)==None: layout.addWidget(Label)
     else: layout.addWidget(Label,row,column)
     Label.setFont(FONT)
-    return Label
 
-def genLineEdit(layout:QtWidgets.QBoxLayout, row:int=None, column:int=None):
-    LineEdit=QtWidgets.QLineEdit()
-    if (row,column)==(None,None): layout.addWidget(LineEdit)
-    else: layout.addWidget(LineEdit,row,column)
-    LineEdit.setFont(FONT)
-    return LineEdit
+def enterRow(layout:QtWidgets.QBoxLayout, task:taskobject):
 
-def genLine(layout:QtWidgets.QBoxLayout, orientation:str='v', r:int=None, c:int=None):
-    if orientation.lower()=='v':
-        Line=QtWidgets.QFrame()
-        Line.setFrameShape(QtWidgets.QFrame.VLine)
-        if r: layout.addWidget(Line,row=r,column=c,columnspan=4)
-        else: layout.addWidget(Line)
-    if orientation.lower()=='h':
-        Line=QtWidgets.QFrame()
-        Line.setFrameShape(QtWidgets.QFrame.HLine)
-        layout.addWidget(Line)
+    genLabel(str(task.slno),layout,task.slno,0)
+    genCheckbox(task,layout,task.slno,2)
+    genLabel(str(task.priority),layout,task.slno,4)
+    genLabel(str(task.DueDate),layout,task.slno,6)
+    genLabel(task.folder,layout,task.slno,8)
 
-def newWindow(nWindow:QtWidgets.QMainWindow, cWindow:QtWidgets.QMainWindow=None):     #if cWindow is passed, this function will close the cwindow 
-    if cWindow:
-        cWindow.close()
-        nWindow.show()
-    else:
-        nWindow.show()
-
-def enterRow(layout:QtWidgets.QBoxLayout, task:str, priority:int=5, DueDate:datetime.datetime=None, folder:str='',colorhex:str="#888888",
-             spacer:QtWidgets.QSpacerItem=None):
-    slno=newSlno()
-
-    if not spacer:
-        genLabel(str(slno),layout,slno,0)
-        genCheckbox(task,layout,slno,2)
-        genLabel(str(priority),layout,slno,4)
-        genLabel(str(DueDate),layout,slno,6)
-        genLabel(folder,layout,slno,8)
-    else:
-        layout.removeItem(spacer)
-        genLabel(str(slno),layout,slno,0)
-        genCheckbox(task,layout,slno,2)
-        genLabel(str(priority),layout,slno,4)
-        genLabel(str(DueDate),layout,slno,6)
-        genLabel(folder,layout,slno,8)
-        spacer=QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        layout.addItem(spacer,slno+1,0)
+def new_windowbttn(current_window: QtWidgets.QMainWindow, new_window: QtWidgets.QMainWindow):
+    if not new_window.isVisible():  # Check if the window is not currently visible
+        new_window.show()  # Show the new window
     
-def newSlno():
-    L=t.fetchall()
-    return L[-1].slno +1
-
-
-def loadUI(main_layout:QtWidgets.QLayout, Tlayout:QtWidgets.QLayout, Flayout:QtWidgets.QLayout):   #This function is not yet complete. Kindly ignore
+def loadUI(main_layout:QtWidgets.QLayout, layout:QtWidgets.QLayout):   
     #Tasks
     Tasks=t.fetchall()
     for i in Tasks:
-        enterRow(Tlayout,i[2],i[3],i[4],i[5])
-
-    """Folders=t.fetchFolders()
-    for i in Folders:
-        f=i[0]  
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM Tasks WHERE Folder=%s",(f,))
-            r=cur.fetchall()
-            for i in r:
-                enterRow(Flayout)"""
+        enterRow(layout,i[2],i[3],i[4],i[5])
  
 """Have to do the same for folders too"""
-
-
-#This function is ALSO not yet completed. Kindly ignore
-'''
-def createlayout(Mainwindow:QtWidgets.QMainWindow,widget:QtWidgets.QWidget):
-    m=Mainwindow
-    Layout=QtWidgets.QGridLayout()
-    
-    if type(widget)==QtWidgets.QStackedWidget:
-        m.page1 = QtWidgets.QWidget()
-        
-        Headingfont=QtGui.QFont()
-        Headingfont.setBold(True)
-        Headingfont.setPointSize(10)
-
-        slno=genLabel("slno",Layout,0,0)
-        genLine(Layout,r=0,c=1)
-        slno.setFont(Headingfont)
-        
-
-        Task=genLabel("Task",Layout,0,2)
-        genLine(Layout,r=0,c=13)
-        Task.setFont(Headingfont)
-        
-
-        P=genLabel("Priority",Layout,0,4)
-        genLine(Layout,r=0,c=5)
-        P.setFont(Headingfont)
-       
-
-        dt=genLabel("Due Date",Layout,0,6)
-        genLine(Layout,r=0,c=7)
-        dt.setFont(Headingfont)
-        
-
-        f=genLabel("Folder",Layout,0,8)
-        f.setFont(Headingfont)
-
-        addSpacer(Layout,1,0)
-
-        m.page1.setLayout(Layout)
-
-        #Adding page to the stacked widget
-        widget.addWidget(m.page1)
-
-        
-def addSpacer(Layout:QtWidgets.QBoxLayout,row:int=None,column:int=None):
-    spacer= QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-    Layout.addItem(spacer)
-    if (row,column)==(None,None): Layout.addItem(spacer)
-    else: Layout.addItem(spacer,row,column)
-'''
